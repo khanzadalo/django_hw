4955d970ed4c94199abe590591697b57d26fbe97
khanzadalo <khanzadaomorova@gmail.com>
2024-01-26 18:04:43 +0600

    corrected the name of the folder for photos

--- product/models.py
+++ product/models.py
@@ -9,7 +9,7 @@ class Category(models.Model):
 
 
 class Product(models.Model):
-    photo = models.ImageField(upload_to="post_photos/%Y/%m/%d", null=True)  # ImageField - поле для загрузки изображения
+    photo = models.ImageField(upload_to="product_photos/%Y/%m/%d", null=True)  # ImageField - поле для загрузки изображения
     title = models.CharField(max_length=100)  # CharField - поле для ввода текста с ограничением по количеству символов
     content = models.TextField(null=True,
                                blank=True)  # TextField - поле для ввода текста без ограничения по количеству символов
